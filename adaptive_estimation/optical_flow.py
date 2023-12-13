85fc5dbd280239574c62c0421ae8c42f679e5611
praveenVnktsh <pveenvenkatesh@gmail.com>
2023-12-13 10:35:13 +0530

    updates

--- adaptive_estimation/optical_flow.py
+++ adaptive_estimation/optical_flow.py
@@ -1,19 +1,13 @@
 import cv2
 import glob
 import numpy as np
-import imageio
-import av
-import os
 from tqdm import tqdm
-from more_itertools import one
-
-os.makedirs('/home/praveenvnktsh/dev/slam_proj/warpsight/ORBSLAM_container/data/videos', exist_ok=True)
+import glob
+import os
 feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
 lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-video_path = '/home/praveenvnktsh/dev/slam_proj/warpsight/ORBSLAM_container/data/videos/schenley_p1.mp4'
-root = video_path.replace('videos/', "").replace('.mp4', '/')
-os.makedirs(root + 'frames', exist_ok=True)
+root = '/home/praveen/dev/slam/warpsight/ORBSLAM_container/GX010035/'
 f = open(root + 'optical_flow.txt', 'w')
 
 frames_to_skip = 0
@@ -22,32 +16,34 @@ prev_frame = None
 
 image_idx = 0
 image_idx = 0
-
-for image_idx in tqdm(range(43500)):
+paths = list(sorted(glob.glob('/home/praveen/dev/slam/warpsight/ORBSLAM_container/GX010035/mav0/cam0/data/*.png')))
+pbar = tqdm(enumerate(paths), total = len(paths))
+for image_idx, filename in pbar:
     
-    cur_frame = cv2.imread('/home/praveenvnktsh/dev/slam_proj/warpsight/ORBSLAM_container/data/schenley_p1/frames/' + str(image_idx).zfill(8) + '.jpg', 0)
+    cur_frame = cv2.imread(filename, 0)
     if cur_frame is None:
         break
     
     if prev_frame is None:
         prev_frame = cur_frame
         continue
-
-    p0 = cv2.goodFeaturesToTrack(cur_frame, mask=None, **feature_params)
-    p1, st, err = cv2.calcOpticalFlowPyrLK(prev_frame, cur_frame, p0, None, **lk_params)
-    
-    good_new = p1[st == 1]
-    good_old = p0[st == 1]
-    
+    try:
+        p0 = cv2.goodFeaturesToTrack(cur_frame, mask=None, **feature_params)
+        p1, st, err = cv2.calcOpticalFlowPyrLK(prev_frame, cur_frame, p0, None, **lk_params)
+        
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
+        
+        mean_disp = np.mean(
+            np.linalg.norm(good_new - good_old, axis = 1)
+        )
+    except:
+        mean_disp = 100
     viz_frame = prev_frame.copy()
     viz_frame = cv2.cvtColor(viz_frame, cv2.COLOR_GRAY2BGR)
-    mean_disp = np.mean(
-        np.linalg.norm(good_new - good_old, axis = 1)
-    )
-
     # mean_disp = np.mean(dists)
-    write_path = 'frames/' + str(image_idx).zfill(8) + '.jpg'
-    f.write(write_path + '\t' +  str(mean_disp) + '\n')
+    name = os.path.basename(filename)
+    f.write(name + '\t' +  str(mean_disp) + '\n')
     image_idx += 1
 
 f.close()
---
+++ adaptive_estimation/select_frames_from_flow.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+
+with open('/home/praveen/dev/slam/warpsight/ORBSLAM_container/GX010035/optical_flow.txt', 'r') as f:
+
+    
+
+    data = f.readlines()
+
+num_frames = len(data)
+f = open('/home/praveen/dev/slam/warpsight/ORBSLAM_container/GX010035/timestamps/timestamps_optical_flow.txt', 'w')
+
+ts = []
+disps = []
+for line in data:
+    name, disp = line.split('\t')
+    disp = float(disp)
+    ts.append(name)
+    disps.append(disp)
+
+disps = np.array(disps)
+disps = np.nan_to_num(disps)
+disps /= disps.max()
+disps = 1 - disps
+
+max_frames_to_skip = 15
+i = 0
+while i < len(disps):
+    f.write(ts[i].split('.')[0] + '\n')
+    if i < 3600:
+        frames_to_skip = 8
+    else:
+        frames_to_skip = int((disps[i]) * max_frames_to_skip ) + 1
+    print(disps[i], frames_to_skip)
+
+    i += frames_to_skip
+
